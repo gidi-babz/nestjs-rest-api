66f8b2af7a8cbfb705cf6615fa166a6c833abf80
Babatunde Olowoyeye <babatunde.olowoyeye@unitedcapitalplcgroup.com>
2024-01-11 20:23:49 +0100

    more services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,22 +4,65 @@ import { Injectable } from '@nestjs/common';
 export class UsersService {
   private users = [
     {
-      id: '',
+      id: 1,
       name: 'John Smith',
       email: 'john.smith@gmail.com',
-      role: 'Admin',
+      role: 'INTERN',
     },
     {
-      id: '',
+      id: 2,
       name: 'John Doe',
       email: 'john.doe@gmail.com',
-      role: 'Compliance',
+      role: 'COMPLIANCE',
     },
     {
-      id: '',
+      id: 3,
       name: 'John Terry',
       email: 'john.T@gmail.com',
-      role: 'Finance',
+      role: 'FINANCE',
     },
   ];
+
+  findAllUsers(role?: 'ADMIN' | 'COMPLIANCE' | 'INTERN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOneUser(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    console.log(user);
+  }
+
+  createUser(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'COMPLIANCE' | 'ADMIN';
+  }) {
+    const userByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: userByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  updateUser(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'COMPLIANCE' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+    return this.findOneUser(id);
+  }
 }
