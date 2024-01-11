49ab64600bdbfc8e8a3c27e17b172d862b9cf300
Babatunde Olowoyeye <babatunde.olowoyeye@unitedcapitalplcgroup.com>
2024-01-11 16:00:11 +0100

    user controller - http requests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() // GET /users or /users?role=value
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findAllUsers(@Query('role') role?: 'ADMIN' | 'COMPLIANCE') {
+    return [];
+  }
+
+  @Get(':id') // GET /user/:id
+  findOneUser(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /user
+  createUser(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /user/:id
+  updateUser(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /user/:id
+  deleteUser(@Param('id') id: string) {
+    return `user with id ${id} has been successfully deleted`;
+  }
+}
